8161d3a4510b5bfc10de309eaad2a79ccf0b8cf2
Tuyen Pham <tuh39709@temple.edu>
2021-03-12 17:43:29 -0500

    add header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCwl1COgzJ-Glbb_7YOQ2qysbCpPoxlTZc",
+  authDomain: "clone-6ad9b.firebaseapp.com",
+  projectId: "clone-6ad9b",
+  storageBucket: "clone-6ad9b.appspot.com",
+  messagingSenderId: "834598763864",
+  appId: "1:834598763864:web:a214308e234f9e8186f92d",
+  measurementId: "G-K2EE1YLMN4",
+};
