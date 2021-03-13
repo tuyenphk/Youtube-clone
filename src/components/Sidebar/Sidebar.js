21eb05443f7f6514bac6852305f973b46dcd5840
Tuyen Pham <tuh39709@temple.edu>
2021-03-12 23:03:19 -0500

    style for sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import HomeIcon from '@material-ui/icons/Home';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
+
 import './Sidebar.css'
+import SidebarRow from './SidebarRow'
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Sidebar is here</h1>
+            <SidebarRow Icon={HomeIcon} title="Home" />
+            <SidebarRow Icon={WhatshotIcon} title="Trending" />
+            <SidebarRow Icon={SubscriptionsIcon} title="Subscriptions" />
         </div>
     )
 }
---
+++ src/components/Sidebar/SidebarRow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Sidebar.css'
+
+function SidebarRow({Icon, title}) {
+    return (
+        <div className="sidebarRow">
+            <Icon className="sidebarRow-icon" />
+           <h2 className="sidebarRow-title">{title}</h2> 
+        </div>
+    )
+}
+
+export default SidebarRow
