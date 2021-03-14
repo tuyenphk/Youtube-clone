2b64fc1e19128a29826027b66661683e5a2dc582
Tuyen Pham <tuh39709@temple.edu>
2021-03-13 20:22:53 -0500

    search page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
 import Video from './components/Video/Video'
+import SearchPage from './components/SearchPage/SearchPage'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import './App.css'
@@ -12,7 +13,12 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/search/:searchTerm"> </Route>
+          <Route path="/search/:searchTerm">
+            <div className="app-page">
+              <Sidebar />
+              <SearchPage />
+            </div>
+          </Route>
           <Route path="/">
             <div className="app-page">
               <Sidebar />
---
+++ src/components/SearchPage/SearchPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './SearchPage.css'
+import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
+
+function SearchPage() {
+    return (
+        <div className="searchPage">
+            <div className="searchPage-filter">
+                <TuneOutlinedIcon />
+                <h2>FILTER</h2>
+            </div>
+            <hr />
+            
+        </div>
+    )
+}
+
+export default SearchPage
