33203a8bd90432f4e5fa425929696105e1cc3f74
Tuyen Pham <tuh39709@temple.edu>
2021-03-13 20:00:29 -0500

    yutube search

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
 import Video from './components/Video/Video'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      {/*Header*/}
-      <Header />
-      <div className="app-page">
-        {/*Sidebar*/}
-        <Sidebar />
-        {/*Recommended videos*/}
-        <Video />
-      </div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search/:searchTerm"> </Route>
+          <Route path="/">
+            <div className="app-page">
+              <Sidebar />
+              <Video />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Avatar from '@material-ui/core/Avatar'
-
+import {Link} from 'react-router-dom'
 import './Header.css'
 
 function Header() {
+    const [inputSearch, setInputSearch] = useState("")
     return (
         <div className="header">
             <div className="header-left">
                  <MenuIcon />
-                <div className="header-logo">
+                 <Link to="/">
+                    <div className="header-logo">
                     <p style={{fontSize: '20px'}}>Yu
                     <span style={{border: '1px solid black', backgroundColor: 'red', color: 'white',
                                 borderRadius: '15px', padding: '5px'}}>Tube</span>
                     </p>
                 </div>
+                 </Link>
             </div>
             <div className="header-center">
-                 <input placeholder="Search" type="text" />
-                <SearchIcon className="header-button" />
+                 <input value={inputSearch} 
+                        onChange={e => setInputSearch(e.target.value)} 
+                        placeholder="Search" type="text" />
+                <Link to={`/search/${inputSearch}`}>
+                    <SearchIcon className="header-button" />
+                </Link>
+                
             </div>
             <div className="header-right">
                 <VideoCallIcon className="header-icon" />
