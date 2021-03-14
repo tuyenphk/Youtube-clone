15038cfc68dfaafd7b4eb48d6dfb8c01fb6fe4f1
Tuyen Pham <tuh39709@temple.edu>
2021-03-13 19:28:21 -0500

    recommended videos fully added

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -23,6 +23,7 @@ function Video() {
                     channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
                     channel="Best Ever Food Review Show"
                     image="https://i.ibb.co/5KY6k4d/3.png"
+                    link="https://www.youtube.com/watch?v=767xPIjTm2A"
                 />
                  <VideoCard 
                     title="$2 PHO vs $100 PHO - Northern VS Southern Pho!"
@@ -31,6 +32,7 @@ function Video() {
                     channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
                     channel="Best Ever Food Review Show"
                     image="https://i.ibb.co/tP682DF/4.png"
+                    link="https://www.youtube.com/watch?v=ftsQYS1fkOs"
                 />
                  <VideoCard 
                     title="Best BREAKFAST in Saigon for CHEAP! Can you even call this breakfast??"
@@ -39,6 +41,7 @@ function Video() {
                     channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
                     channel="Best Ever Food Review Show"
                     image="https://i.ibb.co/17Q9Pkn/5.png"
+                    link="https://www.youtube.com/watch?v=kQj1lIQvYPE"
                 />
                  <VideoCard 
                     title="CHEAP and TASTY STREET FOOD for under $1 | Vietnamese Street Food at Turtle Lake, Saigon"
@@ -47,6 +50,70 @@ function Video() {
                     channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
                     channel="Best Ever Food Review Show"
                     image="https://i.ibb.co/D1P9V3N/6.png"
+                    link="https://www.youtube.com/watch?v=XKGPZwQgeQg"
+                />
+                 <VideoCard 
+                    title="Late Night Street Food!! Bizarre Bites on Vietnam’s Wild Night Life Street!!"
+                    views="2M Views"
+                    timestamp="1 month ago"
+                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
+                    channel="Best Ever Food Review Show"
+                    image="https://i.ibb.co/hLJRJ0F/7.png"
+                    link="https://www.youtube.com/watch?v=YSPqIq0uVUA"
+                />
+                 <VideoCard 
+                    title="RARE Vietnamese Mountain Food in the Most Colorful Market in the World! - Bac Ha Market"
+                    views="5.3M Views"
+                    timestamp="2 months ago"
+                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
+                    channel="Best Ever Food Review Show"
+                    image="https://i.ibb.co/QJLbZ0C/Capture.png"
+                    link="https://www.youtube.com/watch?v=13oOInTY2ik"
+                />
+                 <VideoCard 
+                    title="$5 Oyster VS $100 Oyster w/ Vietnam's OYSTER KING!!"
+                    views="1.6M Views"
+                    timestamp="2 months ago"
+                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
+                    channel="Best Ever Food Review Show"
+                    image="https://i.ibb.co/5G32WSg/1.png"
+                    link="https://www.youtube.com/watch?v=sLLRcoB_DsU"
+                />
+                 <VideoCard 
+                    title="500 Pounds SOLD in 2 Hours!! Why Folks are Dying for this STREET FOOD!!!"
+                    views="1.6M Views"
+                    timestamp="3 weeks ago"
+                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
+                    channel="Best Ever Food Review Show"
+                    image="https://i.ibb.co/WHwkwVr/2.png"
+                    link="https://www.youtube.com/watch?v=C0RVdAcUJLU"
+                />
+                 <VideoCard 
+                    title="Magical FLOATING MARKET TOUR in Cai Rang, Vietnam! (Bun Thit Nuong and Water Banh Mi??)"
+                    views="2.7M Views"
+                    timestamp="2 months ago"
+                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
+                    channel="Best Ever Food Review Show"
+                    image="https://i.ibb.co/DV9zQfz/3.png"
+                    link="https://www.youtube.com/watch?v=3l2-BVCSXJ0"
+                />
+                 <VideoCard 
+                    title="10 Foods Under $1 in Hanoi, Vietnam - Street Food Dollar Menu"
+                    views="2.6M Views"
+                    timestamp="3 months ago"
+                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
+                    channel="Best Ever Food Review Show"
+                    image="https://i.ibb.co/yymK7NS/4.png"
+                    link="https://www.youtube.com/watch?v=GL3mfWvsF24"
+                />
+                 <VideoCard 
+                    title="Vietnam's Seafood NIGHTMARE!!! Extreme Food Tour in Saigon!!"
+                    views="4.3M Views"
+                    timestamp="25 days ago"
+                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
+                    channel="Best Ever Food Review Show"
+                    image="https://i.ibb.co/B4CJ6kv/5.png"
+                    link="https://www.youtube.com/watch?v=6lCkSKn2SwY"
                 />
                 
             </div>
