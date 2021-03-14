d6119e15274de8232ce3695e142beae39261d7e2
Tuyen Pham <tuh39709@temple.edu>
2021-03-14 18:23:08 -0400

    update new version

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import Sidebar from './components/Sidebar/Sidebar'
-import Video from './components/Video/Video'
-import SearchPage from './components/SearchPage/SearchPage'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import React, {Component} from 'react';
+import Home from './containers/Home/Home';
+import {AppLayout} from './components/AppLayout/AppLayout';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import Watch from './containers/Watch/Watch';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {youtubeLibraryLoaded} from './store/actions/api';
+import Trending from './containers/Trending/Trending';
+import Search from './containers/Search/Search';
 
-import './App.css'
+const API_KEY = "AIzaSyBZLgEMThCeTygrVrJ8lEZVVYJ7ym-Eb7A";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Header />
+class App extends Component {
+  render() {
+    return (
+      <AppLayout>
         <Switch>
-          <Route path="/search/:searchTerm">
-            <div className="app-page">
-              <Sidebar />
-              <SearchPage />
-            </div>
-          </Route>
-          <Route path="/">
-            <div className="app-page">
-              <Sidebar />
-              <Video />
-            </div>
-          </Route>
+          <Route path="/feed/trending" component={Trending}/>
+          <Route path="/results" render={() => <Search key={this.props.location.key}/>}/>
+          <Route path="/watch" render={() => <Watch key={this.props.location.key}/>}/>
+          <Route path="/" component={Home}/>
         </Switch>
-      </Router>
-    </div>
-  );
+      </AppLayout>
+    );
+  }
+  componentDidMount() {
+    this.loadYoutubeApi();
+  }
+
+  loadYoutubeApi() {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    script.onload = () => {
+      window.gapi.load('client', () => {
+        window.gapi.client.setApiKey(API_KEY);
+        window.gapi.client.load('youtube', 'v3', () => {
+          this.props.youtubeLibraryLoaded();
+        });
+      });
+    };
+
+    document.body.appendChild(script);
+  }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({youtubeLibraryLoaded}, dispatch);
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,124 +1,21 @@
-import React from 'react'
-import './Video.css'
-import VideoCard from './VideoCard.js'
+import React from 'react';
+import './Video.scss';
 
-function Video() {
-    return (
-        <div className="video">
-            <h2>Recommended</h2>
-            <div className="display">
-                <VideoCard 
-                    title="10 Street Foods UNDER $1 in Saigon, Vietnam!!! Street Food Dollar Menu 2!!"
-                    views="2.6M Views"
-                    timestamp="3 days ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/k1NYbzT/2.png"
-                    link="https://www.youtube.com/watch?v=c_erjcMFT1M"
-                />
-                <VideoCard 
-                    title="Vietnam $100 Street Food Challenge!! Best Street Food in Saigon!!!"
-                    views="6.5M Views"
-                    timestamp="6 days ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/5KY6k4d/3.png"
-                    link="https://www.youtube.com/watch?v=767xPIjTm2A"
-                />
-                 <VideoCard 
-                    title="$2 PHO vs $100 PHO - Northern VS Southern Pho!"
-                    views="7.7M Views"
-                    timestamp="15 days ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/tP682DF/4.png"
-                    link="https://www.youtube.com/watch?v=ftsQYS1fkOs"
-                />
-                 <VideoCard 
-                    title="Best BREAKFAST in Saigon for CHEAP! Can you even call this breakfast??"
-                    views="1.6M Views"
-                    timestamp="18 days ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/17Q9Pkn/5.png"
-                    link="https://www.youtube.com/watch?v=kQj1lIQvYPE"
-                />
-                 <VideoCard 
-                    title="CHEAP and TASTY STREET FOOD for under $1 | Vietnamese Street Food at Turtle Lake, Saigon"
-                    views="2M Views"
-                    timestamp="25 days ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/D1P9V3N/6.png"
-                    link="https://www.youtube.com/watch?v=XKGPZwQgeQg"
-                />
-                 <VideoCard 
-                    title="Late Night Street Food!! Bizarre Bites on Vietnam’s Wild Night Life Street!!"
-                    views="2M Views"
-                    timestamp="1 month ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/hLJRJ0F/7.png"
-                    link="https://www.youtube.com/watch?v=YSPqIq0uVUA"
-                />
-                 <VideoCard 
-                    title="RARE Vietnamese Mountain Food in the Most Colorful Market in the World! - Bac Ha Market"
-                    views="5.3M Views"
-                    timestamp="2 months ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/QJLbZ0C/Capture.png"
-                    link="https://www.youtube.com/watch?v=13oOInTY2ik"
-                />
-                 <VideoCard 
-                    title="$5 Oyster VS $100 Oyster w/ Vietnam's OYSTER KING!!"
-                    views="1.6M Views"
-                    timestamp="2 months ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/5G32WSg/1.png"
-                    link="https://www.youtube.com/watch?v=sLLRcoB_DsU"
-                />
-                 <VideoCard 
-                    title="500 Pounds SOLD in 2 Hours!! Why Folks are Dying for this STREET FOOD!!!"
-                    views="1.6M Views"
-                    timestamp="3 weeks ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/WHwkwVr/2.png"
-                    link="https://www.youtube.com/watch?v=C0RVdAcUJLU"
-                />
-                 <VideoCard 
-                    title="Magical FLOATING MARKET TOUR in Cai Rang, Vietnam! (Bun Thit Nuong and Water Banh Mi??)"
-                    views="2.7M Views"
-                    timestamp="2 months ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/DV9zQfz/3.png"
-                    link="https://www.youtube.com/watch?v=3l2-BVCSXJ0"
-                />
-                 <VideoCard 
-                    title="10 Foods Under $1 in Hanoi, Vietnam - Street Food Dollar Menu"
-                    views="2.6M Views"
-                    timestamp="3 months ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/yymK7NS/4.png"
-                    link="https://www.youtube.com/watch?v=GL3mfWvsF24"
-                />
-                 <VideoCard 
-                    title="Vietnam's Seafood NIGHTMARE!!! Extreme Food Tour in Saigon!!"
-                    views="4.3M Views"
-                    timestamp="25 days ago"
-                    channelImg="https://i.ibb.co/378j42C/unnamed-1.jpg"
-                    channel="Best Ever Food Review Show"
-                    image="https://i.ibb.co/B4CJ6kv/5.png"
-                    link="https://www.youtube.com/watch?v=6lCkSKn2SwY"
-                />
-                
-            </div>
-        </div>
-    )
+const BASE_EMBED_URL = 'https://www.youtube.com/embed/';
+
+export function Video(props) {
+  if(!props.id) {
+    return null;
+  }
+  //const embedUrl = `${BASE_EMBED_URL}${props.id}?autoplay=1`;
+  const embedUrl = `${BASE_EMBED_URL}${props.id}`;
+  return (
+    <div className='video-container'>
+      <div className="video">
+        <iframe className='video-player' src={embedUrl} frameBorder='0'
+                allow='autoplay; encrypted-media' allowFullScreen title='video' />
+      </div>
+
+    </div>
+  );
 }
-
-export default Video
